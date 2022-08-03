c3dbd116db4acd24d9a657ee9f121b1c4dc00262
Mike Casarona <casaromi@users.noreply.github.com>
2022-08-03 16:27:42 -0400

    img2

--- LayeredImageViewer2Full/Assets/WebLoader.cs
+++ LayeredImageViewer2Full/Assets/WebLoader.cs
@@ -10,24 +10,42 @@ using System.Collections;
         "ImageURL" : "..."
       }
 */
-
 public struct Data
 {
+    //Set 1
     public string Name;
     public string ImageURL;
+
+    //Set 2
+    public string Name2;
+    public string ImageURL2;
+
+    //Set 3
+    public string Name3;
+    public string ImageURL3;
 }
 
-public class WebLoader : MonoBehaviour {
+public class WebLoader : MonoBehaviour
+{
+    //Set 1
     [SerializeField] Text uiNameText;
     [SerializeField] RawImage uiRawImage;
 
+    //Set 2
+    [SerializeField] Text uiNameText2;
+    [SerializeField] RawImage uiRawImage2;
+
+    //Set 3
+    [SerializeField] Text uiNameText3;
+    [SerializeField] RawImage uiRawImage3;
+
     string jsonURL = "https://drive.google.com/uc?export=download&id=18mvcVm6iLaiKgpYi6A3mdp3ww8cSzgD0";
+    
 
     void Start()
     {
         StartCoroutine(GetData(jsonURL));
     }
-
 
     IEnumerator GetData(string url)
     {
@@ -44,17 +62,34 @@ public class WebLoader : MonoBehaviour {
             // success...
             Data data = JsonUtility.FromJson<Data>(request.downloadHandler.text);
 
+
+            //Set 1
             // print data in UI
             uiNameText.text = data.Name;
 
             // Load image:
             StartCoroutine(GetImage(data.ImageURL));
+
+
+            //Set 2
+            // print data in UI
+            uiNameText2.text = data.Name2;
+
+            // Load image:
+            StartCoroutine(GetImage(data.ImageURL2));
+            
+            
+            //Set 3
+            // print data in UI
+            uiNameText3.text = data.Name3;
+
+            // Load image:
+            StartCoroutine(GetImage(data.ImageURL3));
         }
 
         // Clean up any resources it is using.
         request.Dispose();
     }
-
 
     IEnumerator GetImage(string url)
     {
@@ -70,9 +105,14 @@ public class WebLoader : MonoBehaviour {
         {
             //success...
             uiRawImage.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+
+            uiRawImage2.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+
+            uiRawImage3.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
         }
 
         // Clean up any resources it is using.
         request.Dispose();
     }
 }
+
