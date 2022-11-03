aafc20a4d56b48f4370eac2b751139c9f304028c
Mike Casarona <casaromi@users.noreply.github.com>
2022-11-03 10:41:07 -0400

    Code For Web download images
    Web download images using JSON and program incrementing

--- LayeredImageViewer2Full/Assets/WebTest.cs
+++ LayeredImageViewer2Full/Assets/WebTest.cs
@@ -24,8 +24,13 @@ public class WebTest : MonoBehaviour
 {
     //Set 1
     [SerializeField] RawImage uiRawImage;
+    [SerializeField] RawImage uiRawImage2;
+    [SerializeField] RawImage uiRawImage3;
 
     string jsonURL = "https://drive.google.com/uc?export=download&id=10g7MWCq2en2bufGgMC7mY0pvZt2diM0z";
+
+    //Intialize Loop Indexing
+    public int index;
 
 
     //Call JSON File
@@ -35,6 +40,7 @@ public class WebTest : MonoBehaviour
     }
 
 
+    //Method for CALLING Images
     IEnumerator GetConfigData(string url)
     {
         //Data Request
@@ -63,38 +69,60 @@ public class WebTest : MonoBehaviour
 
 
             //Cycle through Images
-            for (int i = 1; i < numImgs; i++)
+            for (int i = 1; i <= numImgs; i++)
             {
                 //Set Current URL and print for debug
                 currentURL = baseURL + i + jpgExt;
                 Debug.Log(currentURL);
 
+                //Set Current Index
+                index = i;
+
                 // Load image
-                StartCoroutine(GetImage(currentURL));
-
-
-                //SET Images
-                IEnumerator GetImage(string url)
-                {
-                    UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
-
-                    yield return request.SendWebRequest();
-
-                    if (request.isNetworkError || request.isHttpError)
-                    {
-                        //error...
-                    }
-                    else
-                    {
-                        //success...
-                        uiRawImage.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
-                    }
-
-                    // Clean up any resources it is using.
-                    request.Dispose();
-                }
-
+                StartCoroutine(GetImage(currentURL, index));
+               
+                // Clean up any resources it is using.
+                request.Dispose();
             }
+        }
+        
+    }
+
+
+
+    //Method for SETTING Images
+    IEnumerator GetImage(string url, int index)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            //error...
+        }
+        else
+        {
+            //success...
+            // uiRawImage.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+
+            //THIS IS EXTRA CODE FOR TESTING
+            if (index == 1)
+            {
+                uiRawImage.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+                Debug.Log("Display 1");
+            }
+            else if (index == 2)
+            {
+                uiRawImage2.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+                Debug.Log("Display 2");
+            }
+            else if (index == 3)
+            {
+                uiRawImage3.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+                Debug.Log("Display 3");
+            }
+
         }
         // Clean up any resources it is using.
         request.Dispose();
