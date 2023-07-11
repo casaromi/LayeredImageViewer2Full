18b2e26365523e6b209356e852e2325a4ad6af67
Mike Casarona <casaromi@users.noreply.github.com>
2023-07-11 12:47:25 -0400

    Working Loading Screen Added

--- LayeredImageViewer2Full/Assets/NetworkManager.cs
+++ LayeredImageViewer2Full/Assets/NetworkManager.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -56,6 +57,200 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinOrCreateRoom(roomSettings.Name, roomOptions, TypedLobby.Default);
     }
 
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player has joined the room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
+*/
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class DefaultRoom
+{
+    public string Name;
+    public int sceneIndex;
+    public int maxPlayer;
+}
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public List<DefaultRoom> defaultRooms;
+    public GameObject roomUI;
+    public GameObject loadingScreen;
+
+    // Update is called once per frame
+    public void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try Connect To Server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Server.");
+        base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("Joined the Lobby");
+
+        roomUI.SetActive(true);
+    }
+
+    public void InitiliazeRoom(int defaultRoomIndex)
+    {
+        DefaultRoom roomSettings = defaultRooms[defaultRoomIndex];
+
+        StartCoroutine(LoadRoomAsync(roomSettings));
+    }
+
+    private IEnumerator LoadRoomAsync(DefaultRoom roomSettings)
+    {
+        loadingScreen.SetActive(true); // Show the loading screen
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(roomSettings.sceneIndex); // Load the room scene asynchronously
+        asyncLoad.allowSceneActivation = false; // Prevent the scene from activating immediately
+
+        // Wait until the scene is fully loaded
+        while (!asyncLoad.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f); // Calculate the progress
+            Debug.Log("Loading progress: " + (progress * 100) + "%");
+
+            // You can update the loading screen UI here if needed
+
+            if (progress >= 1f)
+            {
+                asyncLoad.allowSceneActivation = true; // Allow the scene to activate
+            }
+
+            yield return null;
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player has joined the room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class DefaultRoom
+{
+    public string Name;
+    public int sceneIndex;
+    public int maxPlayer;
+}
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public List<DefaultRoom> defaultRooms;
+    public GameObject roomUI;
+    public GameObject loadingScreen;
+    public Slider loadingBar;
+
+    // Update is called once per frame
+    public void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try Connect To Server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Server.");
+        base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("Joined the Lobby");
+
+        roomUI.SetActive(true);
+    }
+
+    public void InitiliazeRoom(int defaultRoomIndex)
+    {
+        DefaultRoom roomSettings = defaultRooms[defaultRoomIndex];
+
+        StartCoroutine(LoadRoomAsync(roomSettings));
+    }
+
+    private IEnumerator LoadRoomAsync(DefaultRoom roomSettings)
+    {
+        loadingScreen.SetActive(true); // Show the loading screen
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(roomSettings.sceneIndex); // Load the room scene asynchronously
+        asyncLoad.allowSceneActivation = false; // Prevent the scene from activating immediately
+
+        while (!asyncLoad.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f); // Calculate the progress based on time
+
+            if (progress >= 0.9f)
+            {
+                // The progress is close to 90% or higher, so we'll simulate a delay
+                // to give the impression of a gradual loading process
+
+                float timeElapsed = 0f;
+                while (timeElapsed < 1f) // Simulate a 1-second delay
+                {
+                    timeElapsed += Time.deltaTime;
+                    progress = Mathf.Lerp(0.9f, 1f, timeElapsed); // Gradually increase the progress
+
+                    loadingBar.value = progress; // Update the loading bar UI
+
+                    yield return null;
+                }
+
+                asyncLoad.allowSceneActivation = true; // Allow the scene to activate
+            }
+            else
+            {
+                loadingBar.value = progress; // Update the loading bar UI
+            }
+
+            yield return null;
+        }
+    }
 
     public override void OnJoinedRoom()
     {
