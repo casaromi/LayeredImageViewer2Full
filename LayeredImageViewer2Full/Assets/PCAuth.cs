80395b345b9554a49bd5a778ba3f1e34f87a174a
Mike Casarona <casaromi@users.noreply.github.com>
2023-07-22 01:00:47 -0400

    button overflow handled

--- LayeredImageViewer2Full/Assets/PCAuth.cs
+++ LayeredImageViewer2Full/Assets/PCAuth.cs
@@ -418,6 +418,10 @@ public class PCAuth : MonoBehaviour
 
 
 
+
+
+
+/*
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
@@ -785,3 +789,388 @@ public class PCAuth : MonoBehaviour
         PlayerPrefs.Save();
     }
 }
+*/
+
+
+
+
+
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using TMPro;
+
+public class PCAuth : MonoBehaviour
+{
+    public InputField Email;
+    public InputField Password;
+    public Button submitButton;
+
+    public Text resultText;
+
+    // Variables to store the data from PHP
+    public string fName;
+    public List<string> modelNames;
+    public List<string> jsonLinks;
+    public List<string> creationDateTimes;
+
+    public GameObject HeaderFeild;
+    public GameObject EmailFeild;
+    public GameObject PasswordFeild;
+    public GameObject sButton;
+    public GameObject ResultText;
+    public GameObject Invalid;
+    public GameObject RoomUI;
+
+    public GameObject ModelMenu;
+
+
+    public GameObject instruct;
+    public GameObject JoinRoom;
+
+    public GameObject ButtonPrefab;
+    public Transform ButtonParent;
+
+    public GameObject Holder;
+
+
+    //BUTTONS Per-Page
+    private int currentPage = 0;
+    private int buttonsPerPage = 4;
+    private List<GameObject> instantiatedButtons = new List<GameObject>();
+
+    public static string selectedJsonLink;
+
+    public static string firstName;
+    public static string userEmail;
+    public static string userPassword;
+
+
+    public Button AllButton;
+    public Button NumberButton;
+    public Button AtoGButton;
+    public Button HtoNButton;
+    public Button OtoZButton;
+
+
+    private string phpURL = "https://davidjoiner.net/~confocal/PCuAuth.php";
+
+    private enum FilterType
+    {
+        All,
+        Number,
+        AtoG,
+        HtoN,
+        OtoZ
+    }
+
+    private FilterType currentFilter = FilterType.All;
+
+    public void CallAuth()
+    {
+        StartCoroutine(Register());
+    }
+
+    IEnumerator Register()
+    {
+        modelNames.Clear();
+        jsonLinks.Clear();
+        ClearButtons();
+
+        WWWForm form = new WWWForm();
+        if (string.IsNullOrEmpty(userEmail) && string.IsNullOrEmpty(userPassword))
+        {
+            form.AddField("Email", Email.text);
+            form.AddField("Password", Password.text);
+
+            Debug.Log("!!!NEW USER!!!");
+        }
+        else
+        {
+            // Retrieve the data from PlayerPrefs
+            form.AddField("Email", userEmail);
+            form.AddField("Password", userPassword);
+
+            Debug.Log("!?!? Welcome back to lobby");
+            Debug.Log(userEmail);
+            Debug.Log(userPassword);
+        }
+
+        UnityWebRequest request = UnityWebRequest.Post(phpURL, form);
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError("Web request error: " + request.error);
+        }
+        else
+        {
+            string response = request.downloadHandler.text;
+            resultText.text = response;
+
+            string[] lines = response.Split('\n');
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("FName: "))
+                {
+                    fName = line.Substring(7);
+                }
+                else if (line.StartsWith("ModelName: "))
+                {
+                    string modelName = line.Substring(11);
+                    modelNames.Add(modelName);
+                }
+                else if (line.StartsWith("JsonLink: "))
+                {
+                    string jsonLink = line.Substring(10);
+                    jsonLinks.Add(jsonLink);
+                }
+                else if (line.StartsWith("CreationDateTime: "))
+                {
+                    string creationDateTime = line.Substring(18);
+                    creationDateTimes.Add(creationDateTime);
+                }
+            }
+
+            modelNames.Sort();
+
+            if (fName != null && fName != "")
+            {
+                // Call StoreUserInfo to store user information in PlayerPrefs
+                StoreUserInfo(0);
+
+
+                HeaderFeild.SetActive(false);
+                EmailFeild.SetActive(false);
+                PasswordFeild.SetActive(false);
+                sButton.SetActive(false);
+                Invalid.SetActive(false);
+                RoomUI.SetActive(false);
+
+                instruct.SetActive(false);
+                JoinRoom.SetActive(true);
+
+                ModelMenu.SetActive(true);
+
+
+                Holder.SetActive(true);
+
+                DisplayButtons();
+            }
+            else
+            {
+                Debug.Log("Invalid email or password");
+                Invalid.SetActive(true);
+                RoomUI.SetActive(false);
+            }
+        }
+    }
+
+
+
+
+    
+
+    private void DisplayButtons()
+    {
+        ClearButtons();
+
+        List<string> filteredModelNames = GetFilteredModelNames();
+        int startIndex = currentPage * buttonsPerPage;
+        int endIndex = Mathf.Min(startIndex + buttonsPerPage, filteredModelNames.Count);
+
+        if (startIndex >= filteredModelNames.Count)
+        {
+            currentPage = (filteredModelNames.Count - 1) / buttonsPerPage;
+            startIndex = currentPage * buttonsPerPage;
+            endIndex = Mathf.Min(startIndex + buttonsPerPage, filteredModelNames.Count);
+        }
+
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            string modelName = filteredModelNames[i];
+            string creationDateTimeString = creationDateTimes[i];
+
+            // Parse the CreationDateTime string into a DateTime object
+            if (DateTime.TryParse(creationDateTimeString, out DateTime creationDateTime))
+            {
+                // Format the date as "month, day, year"
+                string formattedDate = creationDateTime.ToString("MMMM dd, yyyy");
+
+                GameObject buttonObj = Instantiate(ButtonPrefab, ButtonParent);
+                instantiatedButtons.Add(buttonObj);
+                Button button = buttonObj.GetComponent<Button>();
+                TMP_Text buttonText = buttonObj.GetComponentInChildren<TMP_Text>();
+
+                // Use rich text formatting to apply different colors and font size to modelName and formattedDate
+                string formattedText = $"<size=20><color=blue>{modelName}</color></size>\n<size=10><color=red>{formattedDate}</color></size>";
+                buttonText.text = formattedText;
+
+                // Set the text overflow mode to truncate
+                buttonText.overflowMode = TextOverflowModes.Truncate;
+
+                // Set the margin for the truncation area (left, right, top, bottom)
+                buttonText.margin = new Vector4(20f, 5f, 20f, 5f); // Adjust the values as needed
+
+                int index = i;
+                button.onClick.AddListener(() => SelectJsonLink(index));
+            }
+            else
+            {
+                Debug.LogWarning("Invalid date format: " + creationDateTimeString);
+            }
+        }
+
+        // Set the color of the filter buttons based on the currentFilter
+        AllButton.GetComponent<Image>().color = currentFilter == FilterType.All ? Color.blue : Color.white;
+        NumberButton.GetComponent<Image>().color = currentFilter == FilterType.Number ? Color.blue : Color.white;
+        AtoGButton.GetComponent<Image>().color = currentFilter == FilterType.AtoG ? Color.blue : Color.white;
+        HtoNButton.GetComponent<Image>().color = currentFilter == FilterType.HtoN ? Color.blue : Color.white;
+        OtoZButton.GetComponent<Image>().color = currentFilter == FilterType.OtoZ ? Color.blue : Color.white;
+    }
+
+
+
+
+
+    private void ClearButtons()
+    {
+        foreach (GameObject buttonObj in instantiatedButtons)
+        {
+            Destroy(buttonObj);
+        }
+
+        instantiatedButtons.Clear();
+    }
+
+    private List<string> GetFilteredModelNames()
+    {
+        List<string> filteredModelNames = new List<string>();
+
+        switch (currentFilter)
+        {
+            case FilterType.All:
+                filteredModelNames = modelNames;
+                break;
+            case FilterType.Number:
+                filteredModelNames = modelNames.FindAll(modelName => char.IsNumber(modelName[0]));
+                break;
+            case FilterType.AtoG:
+                filteredModelNames = modelNames.FindAll(modelName => modelName[0] >= 'A' && modelName[0] <= 'G');
+                break;
+            case FilterType.HtoN:
+                filteredModelNames = modelNames.FindAll(modelName => modelName[0] >= 'H' && modelName[0] <= 'N');
+                break;
+            case FilterType.OtoZ:
+                filteredModelNames = modelNames.FindAll(modelName => modelName[0] >= 'O' && modelName[0] <= 'Z');
+                break;
+        }
+
+        return filteredModelNames;
+    }
+
+    private void SelectJsonLink(int index)
+    {
+        selectedJsonLink = jsonLinks[index];
+        Debug.Log("Selected JsonLink: " + selectedJsonLink);
+        RoomUI.SetActive(true);
+    }
+
+
+    private void StoreUserInfo(int index)
+    {
+        if (string.IsNullOrEmpty(userEmail) && string.IsNullOrEmpty(userPassword) && string.IsNullOrEmpty(firstName))
+        {
+            Debug.Log("!!!!!!!!!");
+
+            firstName = fName;
+            userEmail = Email.text;
+            userPassword = Password.text;
+
+            // Save the data using PlayerPrefs
+            PlayerPrefs.SetString("FirstName", firstName);
+            PlayerPrefs.SetString("UserEmail", userEmail);
+            PlayerPrefs.SetString("UserPassword", userPassword);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            Debug.Log("!!!!!User Already logged in");
+        }
+    }
+
+
+    public void NextPage()
+    {
+        currentPage++;
+        DisplayButtons();
+    }
+
+    public void PreviousPage()
+    {
+        currentPage--;
+        if (currentPage < 0)
+            currentPage = 0;
+        DisplayButtons();
+    }
+
+    public void ShowAllModels()
+    {
+        currentFilter = FilterType.All;
+        currentPage = 0;
+        DisplayButtons();
+    }
+
+    public void ShowModelsStartingWithNumber()
+    {
+        currentFilter = FilterType.Number;
+        currentPage = 0;
+        DisplayButtons();
+    }
+
+    public void ShowModelsStartingWithAtoG()
+    {
+        currentFilter = FilterType.AtoG;
+        currentPage = 0;
+        DisplayButtons();
+    }
+
+    public void ShowModelsStartingWithHtoN()
+    {
+        currentFilter = FilterType.HtoN;
+        currentPage = 0;
+        DisplayButtons();
+    }
+
+    public void ShowModelsStartingWithOtoZ()
+    {
+        currentFilter = FilterType.OtoZ;
+        currentPage = 0;
+        DisplayButtons();
+    }
+
+    private void OnDestroy()
+    {
+        ClearButtons();
+    }
+
+
+
+    // Add this method to clear player preferences on application quit
+    private void OnApplicationQuit()
+    {
+        ClearPlayerPrefs();
+    }
+
+    // Method to clear player preferences
+    private void ClearPlayerPrefs()
+    {
+        // Clear all player preferences
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+    }
+}
+
