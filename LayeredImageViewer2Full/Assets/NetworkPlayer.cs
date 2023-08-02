cb8bdb9c586ae49471b6dd0b2d6e4acc2be31afb
Mike Casarona <casaromi@users.noreply.github.com>
2023-08-02 16:15:02 -0400

    Name display - TEST!!

--- LayeredImageViewer2Full/Assets/NetworkPlayer.cs
+++ LayeredImageViewer2Full/Assets/NetworkPlayer.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -85,4 +86,113 @@ public class NetworkPlayer : MonoBehaviour
         target.rotation = rigTransform.rotation;
     }
 }
+
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
+using Unity.XR.CoreUtils;
+using TMPro;
+using ExitGames.Client.Photon; // Import the correct Hashtable class
+
+public class NetworkPlayer : MonoBehaviourPun
+{
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+
+    public Animator leftHandAnimator;
+    public Animator rightHandAnimator;
+
+    public TextMeshPro firstNameTextMesh; // Reference to the 3D text prefab for first name
+
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //XRRig rig = FindObjectOfType<XRRig>();
+        XROrigin rig = FindObjectOfType<XROrigin>();
+
+        headRig = rig.transform.Find("Camera Offset/Main Camera");
+        leftHandRig = rig.transform.Find("Camera Offset/LeftHand Controller");
+        rightHandRig = rig.transform.Find("Camera Offset/RightHand Controller");
+
+        if (photonView.IsMine)
+        {
+            foreach (var item in GetComponentsInChildren<Renderer>())
+            {
+                item.enabled = false;
+            }
+
+            // Get the value of the FirstName variable from PlayerPrefs
+            string firstName = PlayerPrefs.GetString("FirstName", "");
+
+            // Set the photonView.Owner.NickName to the firstName
+            PhotonNetwork.LocalPlayer.NickName = firstName;
+
+            // Update the custom property "FirstName" for the local player
+            ExitGames.Client.Photon.Hashtable playerNameProp = new ExitGames.Client.Photon.Hashtable();
+            playerNameProp.Add("FirstName", firstName);
+            PhotonNetwork.LocalPlayer.SetCustomProperties(playerNameProp);
+        }
+        else
+        {
+            // For remote players, get their custom property "FirstName" and update the 3D Text
+            if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("FirstName", out object remoteFirstName))
+            {
+                firstNameTextMesh.text = (string)remoteFirstName;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
+
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.LeftHand), leftHandAnimator);
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.RightHand), rightHandAnimator);
+        }
+    }
+
+    void UpdateHandAnimation(InputDevice targetDevice, Animator handAnimator)
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+    void MapPosition(Transform target, Transform rigTransform)
+    {
+        target.position = rigTransform.position;
+        target.rotation = rigTransform.rotation;
+    }
+}
+
 
