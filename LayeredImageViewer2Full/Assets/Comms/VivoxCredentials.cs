1743fe4c66b5fa1df610b2970e1a50986426902c
Mike Casarona <casaromi@users.noreply.github.com>
2024-03-04 16:06:56 -0500

    Voice 2.0 Test

--- LayeredImageViewer2Full/Assets/Comms/VivoxCredentials.cs
+++ LayeredImageViewer2Full/Assets/Comms/VivoxCredentials.cs
@@ -7,10 +7,10 @@ using VivoxUnity;
 public class VivoxCredentials
 {
     public VivoxUnity.Client client;
-    public Uri server = new Uri("https://mt1s.www.vivox.com/api2");
-    public string issuer = "michae0067-vi50-dev";
-    public string domain = "mt1s.vivox.com";
-    public string tokenKey = "chap306";
+    public Uri server = new Uri("https://mtu1xp-mad.vivox.com");
+    public string issuer = "24743-my_pr-73806-udash";
+    public string domain = "mtu1xp.vivox.com";
+    public string tokenKey = "vKK93FcXEu2ZcUOarqFJk3bkjDpNUZeB";
     public TimeSpan timeSpan = TimeSpan.FromSeconds(90);
 
 
