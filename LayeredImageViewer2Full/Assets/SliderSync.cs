10e7dbb6e8a2f4611da21b28ca0d0f355b1ae59c
Mike Casarona <casaromi@users.noreply.github.com>
2025-04-28 15:15:29 -0400

    Sliders networked?

---
+++ LayeredImageViewer2Full/Assets/SliderSync.cs
@@ -0,0 +1,50 @@
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderSync : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public Slider slider;
+    private float syncedValue;
+
+    void Start()
+    {
+        if (slider == null)
+            slider = GetComponent<Slider>();
+
+        Debug.Log("Am I Master? " + PhotonNetwork.IsMasterClient);
+
+        // Only let local player control the slider if they're the master
+        // slider.interactable = PhotonNetwork.IsMasterClient;
+
+        slider.onValueChanged.AddListener(OnSliderValueChanged);
+    }
+
+    void OnSliderValueChanged(float value)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            syncedValue = value;
+        }
+    }
+
+    void Update()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            slider.value = syncedValue; // Keep synced for remote players
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting && PhotonNetwork.IsMasterClient)
+        {
+            stream.SendNext(syncedValue);
+        }
+        else
+        {
+            syncedValue = (float)stream.ReceiveNext();
+        }
+    }
+}
